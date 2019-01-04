705ce14074ed088ec4fe84a9f60ad22675ab3ba5
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-01-04 19:22:15 +0200

    Initial Commit

---
+++ calculation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculation
+//
+//  Created by Артем on 1/4/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
